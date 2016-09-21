84a35f8ea2f31e611c2b1c7d5b5da81bb05b25a7
Jordan <CJ94571@kroger.com>
2016-09-21 12:30:32 -0400

    Filling out persist, get by id methods

--- h2/src/main/org/h2/server/web/ConnectionInfoDao.java
+++ h2/src/main/org/h2/server/web/ConnectionInfoDao.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
+import org.h2.util.StringUtils;
+
 public class ConnectionInfoDao
 {
 
@@ -69,9 +71,10 @@ public class ConnectionInfoDao
             try
             {
                 if (null != stmt)
-                {  stmt.close();
+                {
+                    stmt.close();
                 }
-                
+
             }
             catch (Exception e)
             {
@@ -82,24 +85,26 @@ public class ConnectionInfoDao
         }
     }
 
-    private boolean saveConnectionInfo(String euid, String connectionName,
-        String connectionDriverName, String connectionUrl, String connectionUser)
+    private void saveConnectionInfo(String euid, String connectionName, String connectionDriverName,
+        String connectionUrl, String connectionUser)
     {
-        boolean result = true;
-
         Connection connection = ConnectionInViewFilter.getConnection();
 
         StringBuilder sqlSb = new StringBuilder(
-            "insert into connection_info (euid, connection_name, connection_driver, connection_url, connection_user) ");
-        sqlSb.append(" (");
-        
-
+            // "insert into connection_info (euid, connection_name, connection_driver,
+            // connection_url, connection_user) Values (");
+            "insert into connection_info (euid, connection_name, connection_driver, connection_url, connection_user) Values (?, ? , ?, ?, ?)");
 
         PreparedStatement stmt = null;
 
         try
         {
             stmt = connection.prepareStatement(sqlSb.toString());
+            stmt.setString(1, euid);
+            stmt.setString(2, connectionName);
+            stmt.setString(3, connectionDriverName);
+            stmt.setString(4, connectionUrl);
+            stmt.setString(5, connectionUser);
         }
         catch (Exception e)
         {
@@ -110,7 +115,7 @@ public class ConnectionInfoDao
         {
             try
             {
-                if (null != stmt )
+                if (null != stmt)
                 {
                     stmt.close();
                 }
@@ -122,16 +127,85 @@ public class ConnectionInfoDao
                 e.printStackTrace(System.out);
             }
         }
+    }
+    
+    
+    private void updateConnectionInfo(Long id, String euid, String connectionName, String connectionDriverName,
+        String connectionUrl, String connectionUser)
+    {
+        
+        ConnectionInfo connectionInfo = getConnectionInfoById(id);
+        
+        if (null == connectionInfo)
+        {
+            throw new RuntimeException("Connection info with id " + id + " was not found.  It may have already been removed.");
+        }
+            
+        if (!StringUtils.equals(euid, connectionInfo.euid))
+        {
+            throw new RuntimeException("Connection Information with id of " + id + " is not owned by " + euid);
+        }
 
+        Connection connection = ConnectionInViewFilter.getConnection();
 
-        return result;
+        StringBuilder sqlSb = new StringBuilder(
+            // "insert into connection_info (euid, connection_name, connection_driver,
+            // connection_url, connection_user) Values (");
+            "insert into connection_info (euid, connection_name, connection_driver, connection_url, connection_user) Values (?, ? , ?, ?, ?)");
+
+        PreparedStatement stmt = null;
+
+        try
+        {
+            stmt = connection.prepareStatement(sqlSb.toString());
+            stmt.setString(1, euid);
+            stmt.setString(2, connectionName);
+            stmt.setString(3, connectionDriverName);
+            stmt.setString(4, connectionUrl);
+            stmt.setString(5, connectionUser);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Error saving connectionInfos for euid:"
+                + euid, e);
+        }
+        finally
+        {
+            try
+            {
+                if (null != stmt)
+                {
+                    stmt.close();
+                }
+            }
+            catch (Exception e)
+            {
+                // Just log it
+                System.out.println("Error closing prepared statement!");
+                e.printStackTrace(System.out);
+            }
+        }
     }
 
 
-    public boolean persistConnectionInfo(Long connectionId, String euid, String connectionName,
+
+    public void persistConnectionInfo(Long connectionId, String euid, String connectionName,
         String connectionDriverName, String connectionUrl, String connectionUser)
     {
-        return true;
+
+
+        if (connectionId == null)
+        {
+            saveConnectionInfo(euid, connectionName, connectionDriverName, connectionUrl,
+                connectionUser);
+        }
+        else
+        {
+            updateConnectionInfo(connectionId, euid, connectionName, connectionDriverName,
+                connectionUrl, connectionUser);
+        }
+
+
     }
 
     public boolean deleteConnectionInfo(Long connectionId, String euid)
@@ -139,5 +213,81 @@ public class ConnectionInfoDao
         return true;
     }
 
+
+    private void addStringParameter(StringBuilder sb, String stringParameter)
+    {
+        sb.append("'");
+        sb.append(stringParameter);
+        sb.append("'");
+    }
+
+
+    private void addAnotherStringParam(StringBuilder sb, String stringParameter)
+    {
+        sb.append(",");
+        addStringParameter(sb, stringParameter);
+    }
+
+
+    public ConnectionInfo getConnectionInfoById(Long id)
+    {
+        PreparedStatement stmt=null;
+        ResultSet rs = null;
+        ConnectionInfo connectionInfo = null;
+        try
+        {
+            Connection connection = ConnectionInViewFilter.getConnection();
+
+            String sql = "select * from connection_info where id = ?";
+            stmt = connection.prepareStatement(sql);
+            stmt.setLong(1, id);
+
+            rs = stmt.executeQuery();
+            if (rs.next())
+            {
+                connectionInfo = new ConnectionInfo();
+                connectionInfo.euid = rs.getString("euid");
+                connectionInfo.driver = rs.getString("connection_driver");
+                connectionInfo.url = rs.getString("connection_url");
+                connectionInfo.user = rs.getString("connection_user");
+                connectionInfo.name = rs.getString("connection_name");
+                connectionInfo.id = rs.getLong("id");
+            }
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Error getting connectionInfos by id:"
+                + id, e);
+        }
+        finally
+        {
+            try
+            {
+                rs.close();
+            }
+            catch (Exception e)
+            {
+                // Just log it
+                System.out.println("Error closing resultset!");
+                e.printStackTrace(System.out);
+            }
+
+            try
+            {
+                if (null != stmt)
+                {
+                    stmt.close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                // Just log it
+                System.out.println("Error closing prepared statement!");
+                e.printStackTrace(System.out);
+            }
+        }
+        return connectionInfo;
+    }
 
 }
